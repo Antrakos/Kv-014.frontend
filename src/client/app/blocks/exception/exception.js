d0a1360daebb3b1c0c2f737f49cfb72064c54fe6
Mononofuu <mononofuu@gmail.com>
2016-06-24 13:50:05 +0300

    exception reworked (#16)

--- src/client/app/blocks/exception/exception.js
+++ src/client/app/blocks/exception/exception.js
@@ -1,27 +1,29 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('blocks.exception')
-    .factory('exception', exception);
+    angular
+        .module('blocks.exception')
+        .factory('exception', exception);
 
-  /* @ngInject */
-  function exception($q, logger) {
-    var service = {
-      catcher: catcher
-    };
-    return service;
+    /* @ngInject */
+    function exception($q, logger) {
+        var service = {
+            catcher: catcher
+        };
+        return service;
 
-    function catcher(e) {
-        var newMessage;
-        console.info(e.data)
-        if (e.data.message) {
-            newMessage = e.data.message;
-        } else if (e.data.errors[0]) {
-            newMessage = e.data.errors[0].validationMessage;
+        function catcher(e) {
+            var newMessage;
+            console.info(e.data)
+            if (e.data.message) {
+                newMessage = e.data.message;
+                logger.error(newMessage);
+            } else if (e.data.errors) {
+                e.data.errors.forEach(function (item) {
+                    logger.error(item.validationMessage)
+                })
+            }
+            return $q.reject(e);
         }
-        logger.error(newMessage);
-        return $q.reject(e);
     }
-  }
 })();
