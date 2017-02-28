e82da580e01f188270d33cde23c89023c19abfb0
Antrakos <taraszubrei@gmail.com>
2017-02-28 17:36:32 +0200

    Delete unnecessary express rest api

--- src/server/app.js
+++ src/server/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var port = process.env.PORT || 8001;
-var four0four = require('./utils/404')();
 
 var environment = process.env.NODE_ENV;
 
@@ -15,8 +14,6 @@ app.use(favicon(__dirname + '/favicon.ico'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
-app.use('/api', require('./routes'));
 
 console.log('About to crank up node');
 console.log('PORT=' + port);
@@ -27,9 +24,6 @@ switch (environment) {
     console.log('** BUILD **');
     app.use(express.static('./build/'));
     // Any invalid calls for templateUrls are under app/* and should return 404
-    app.use('/app/*', function(req, res, next) {
-      four0four.send404(req, res);
-    });
     // Any deep link calls should return index.html
     app.use('/*', express.static('./build/index.html'));
     break;
@@ -38,10 +32,6 @@ switch (environment) {
     app.use(express.static('./src/client/'));
     app.use(express.static('./'));
     app.use(express.static('./tmp'));
-    // Any invalid calls for templateUrls are under app/* and should return 404
-    app.use('/app/*', function(req, res, next) {
-      four0four.send404(req, res);
-    });
     // Any deep link calls should return index.html
     app.use('/*', express.static('./src/client/index.html'));
     break;
